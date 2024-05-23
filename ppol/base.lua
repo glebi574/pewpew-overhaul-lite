368be158efa37d905df9d6c204806c8c64a6d2d8
glebi574 <glebi574@gmail.com>
2024-05-23 20:36:53 +0300

    Optimized `print`

--- ppol/base.lua
+++ ppol/base.lua
@@ -7,8 +7,7 @@ function print(...) -- adds ability to use fx numbers with print and fixes their
       if output[i] == 0fx or output[i] / output[i] == 1 then
         output[i] = string.format(output[i])
       else
-        local v = to_int(output[i] * 4096fx)
-        output[i] = string.format('%s%d.%dfx', v < 0 and '-' or '', abs(v) // 4096, abs(v) % 4096)
+        output[i] = (output[i] < 0fx and '-' or '') .. fx_abs(output[i])
       end
     elseif output[i] == nil then
       output[i] = 'nil'
